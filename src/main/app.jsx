577d12b8afc48001493857b209dfdc0b915f9070
matheus-lima92 <lima.eca@gmail.com>
2018-03-21 23:36:58 -0300

    header component

--- src/main/app.jsx
+++ src/main/app.jsx
@@ -1,8 +1,10 @@
 import '../common/template/dependencies' //arquivos estão conectados, então dependencias se estendem para o app
 import React from 'react'
 
+import Header from '../common/template/header'
+
 export default props => (
 	<div className='wrapper'>
-		<h1>App</h1>
+		<Header/>
 	</div>
 )
