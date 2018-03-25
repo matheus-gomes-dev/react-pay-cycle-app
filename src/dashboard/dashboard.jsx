74e0a8bedcc935d6943321eaf8ef666721d72aec
matheus-lima92 <lima.eca@gmail.com>
2018-03-24 22:58:13 -0300

    getting dashboard state with redux

--- src/dashboard/dashboard.jsx
+++ src/dashboard/dashboard.jsx
@@ -1,18 +1,19 @@
 import React, { Component } from 'react'
-
+import { connect } from 'react-redux'
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
 import ValueBox from '../common/widget/valueBox'
 
 class Dashboard extends Component {
 	render() {
+		const {credit, debt} = this.props.summary //armazenando valores de estado através do redux
 		return (
 			<div>
 				<ContentHeader title="Dashboard" small="Versão 1.0"/>
 				<Content>
 					<div className='row'>
-						<ValueBox cols=' 12 4' color='green' icon='bank' value='R$ 10' text='Total de Créditos'/>
-						<ValueBox cols=' 12 4' color='red' icon='credit-card' value='R$ 10' text='Total de Débitos'/>
+						<ValueBox cols=' 12 4' color='green' icon='bank' value={`R$ ${credit}`} text='Total de Créditos'/>
+						<ValueBox cols=' 12 4' color='red' icon='credit-card' value={`R$ ${debt}`} text='Total de Débitos'/>
 						<ValueBox cols=' 12 4' color='blue' icon='money' value='R$ 0' text='Valor consolidado'/>
 					</div>
 				</Content>
@@ -21,4 +22,5 @@ class Dashboard extends Component {
 	}
 }
 
-export default Dashboard
+const mapStateToProps = state => ({summary: state.dashboard.summary})
+export default connect(mapStateToProps)(Dashboard)
