28ccb8dc05ab28bcb6b77763b714ef34c5626bea
matheus-lima92 <lima.eca@gmail.com>
2018-03-24 21:19:16 -0300

    included router

--- src/main/app.jsx
+++ src/main/app.jsx
@@ -4,13 +4,14 @@ import React from 'react'
 import Header from '../common/template/header'
 import SideBar from '../common/template/sideBar'
 import Footer from '../common/template/footer'
+import Router from './routes'
 
 export default props => (
 	<div className='wrapper'>
 		<Header/>
 		<SideBar/>
 		<div className='content-wrapper'>
-			<h1>Content</h1>
+			<Router/>
 		</div>
 		<Footer/>
 	</div>
