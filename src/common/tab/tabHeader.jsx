69892aafbcdaa09ff5d33bdfe0de39c9177e8ffa
matheus-lima92 <lima.eca@gmail.com>
2018-03-27 17:31:31 -0300

    control of tab component's state'

--- src/common/tab/tabHeader.jsx
+++ src/common/tab/tabHeader.jsx
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+
+import { selectTab } from './tabActions'
 
 class TabHeader extends Component {
 	render() {
+		const selected = this.props.tab.selected === this.props.target
 		return (
-			<li>
-				<a href='' data-toggle='tab' data-target={this.props.target}>
+			<li className={selected ? 'active' : ''}>
+				<a href='javascript:;' data-toggle='tab' data-target={this.props.target} onClick={() => this.props.selectTab(this.props.target)}>
 					<i className={`fa fa-${this.props.icon}`}></i>
 					{this.props.label}
 				</a>
@@ -13,4 +18,6 @@ class TabHeader extends Component {
 	}
 }
 
-export default TabHeader
+const mapStateToProps = state => ({tab: state.tab})
+const mapDispatchToProps = dispatch => bindActionCreators({selectTab}, dispatch)
+export default connect(mapStateToProps, mapDispatchToProps)(TabHeader)
