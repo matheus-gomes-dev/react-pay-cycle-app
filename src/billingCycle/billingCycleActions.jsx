9f54c2ab572fdaa52425cfc0d0e7e53c64da452b
matheus-lima92 <lima.eca@gmail.com>
2018-03-28 21:25:42 -0300

    handling actions after form submit with redux-multi middleware

--- src/billingCycle/billingCycleActions.jsx
+++ src/billingCycle/billingCycleActions.jsx
@@ -1,5 +1,7 @@
 import axios from 'axios'
 import { toastr } from 'react-redux-toastr'
+import {reset as resetForm } from 'redux-form'
+import { showTabs, selectTab } from '../common/tab/tabActions'
 const BASE_URL = 'http://localhost:3003/api'
 
 export function getList() {
@@ -11,14 +13,36 @@ export function getList() {
 }
 
 export function create(values){
-	axios.post(`${BASE_URL}/billingCycles`, values)
-		.then(resp => {
-			toastr.success('Sucesso', 'Operação realizada com sucesso!')
-		})
-		.catch(e => {
-			e.response.data.errors.map(error => toastr.error('Erro', error))
-		})
-	return {
-		type: 'TEMP'
+	//É esperado que action creators retornem sempre ações (https://redux.js.org/basics/actions)
+	//notar que o retorno não é nada mais do que uma função com callback, retornando várias ações.
+	//Notação é um pouco diferente dos demais action creators, mas ver que getList() poderia ser
+	//substituido por getList2() (comentado abaixo) e daria na mesma
+	return actions => {
+		axios.post(`${BASE_URL}/billingCycles`, values)
+			.then(resp => {
+				toastr.success('Sucesso', 'Operação realizada com sucesso!')
+				//posso retornar um array de actions somente porque o middleware redux-multi foi incluído
+				actions([
+					resetForm('billingCycleForm'),
+					getList(),
+					selectTab('tabList'),
+					showTabs('tabList', 'tabCreate')
+				])
+			})
+			.catch(e => {
+				e.response.data.errors.map(error => toastr.error('Erro', error))
+			})
 	}
 }
+
+/*
+export function getList2() {
+    const request = axios.get(`${BASE_URL}/billingCycles`)
+    return function(cb) {
+    	cb({
+	        type: 'BILLING_CYCLES_FETCHED',
+    	    payload: request
+    	})
+    }
+}
+*/
