20499f03ff515e049b70f942a2839a43c58e2bdd
matheus-lima92 <lima.eca@gmail.com>
2018-03-28 22:25:17 -0300

    form layout update

--- src/billingCycle/billingCycleForm.jsx
+++ src/billingCycle/billingCycleForm.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { reduxForm, Field } from 'redux-form'
+import labelAndInput from '../common/form/labelAndInput'
 
 class BillingCycleForm extends Component {
 
@@ -9,9 +10,9 @@ class BillingCycleForm extends Component {
         return (
             <form role='form' onSubmit={handleSubmit}>
                 <div className='box-body'>
-                    <Field name='name' component='input' />
-                    <Field name='month' component='input' />
-                    <Field name='year' component='input' />
+                    <Field name='name' component={labelAndInput} label='Name' cols='12 4' placeholder='Inform name'/>
+                    <Field name='month' component={labelAndInput} label='Month' cols='12 4' placeholder='Inform month' type='number'/>
+                    <Field name='year' component={labelAndInput} label='Year' cols='12 4' placeholder='Inform year' type='number'/>
                 </div>
                 <div className='box-footer'>
                     <button type='submit' className='btn btn-primary'>
