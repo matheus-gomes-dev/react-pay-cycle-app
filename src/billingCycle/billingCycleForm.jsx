9eacd79bd6e594ccb98b9ac5d04c9c8818cc3f51
matheus-lima92 <lima.eca@gmail.com>
2018-04-01 17:05:57 -0300

    controlling deb and credit values

--- src/billingCycle/billingCycleForm.jsx
+++ src/billingCycle/billingCycleForm.jsx
@@ -10,9 +10,18 @@ import Summary from './summary'
 
 class BillingCycleForm extends Component {
 
+    calculateSummary() {
+        const sum = (t, v) => t + v
+        return {
+            sumOfCredits: this.props.credits.map(c => +c.value || 0).reduce(sum),
+            sumOfDebts: this.props.debts.map(d => +d.value || 0).reduce(sum)
+        }
+    }
+
 	render() {
         //handleSubmit fica disponível após redux-form
         const { handleSubmit, readOnly, credits, debts } = this.props
+        const {sumOfCredits, sumOfDebts} = this.calculateSummary()
         console.log(this.props)
         return (
             <form role='form' onSubmit={handleSubmit}>
@@ -20,7 +29,7 @@ class BillingCycleForm extends Component {
                     <Field name='name' component={labelAndInput} label='Name' cols='12 4' placeholder='Inform name' readOnly={readOnly}/>
                     <Field name='month' component={labelAndInput} label='Month' cols='12 4' placeholder='Inform month' type='number' readOnly={readOnly}/>
                     <Field name='year' component={labelAndInput} label='Year' cols='12 4' placeholder='Inform year' type='number' readOnly={readOnly}/>
-                    <Summary credit={1000} debt={100} />
+                    <Summary credit={sumOfCredits} debt={sumOfDebts} />
                     <ItemList cols='12 6' list={credits} readOnly={readOnly} field='credits' legend='Credits'/>
                     <ItemList cols='12 6' list={debts} readOnly={readOnly} field='debts' legend='Debts' showStatus={true}/>
                 </div>
--- src/dashboard/dashboard.jsx
+++ src/dashboard/dashboard.jsx
@@ -23,7 +23,7 @@ class Dashboard extends Component {
 					<div className='row'>
 						<ValueBox cols=' 12 4' color='green' icon='bank' value={`R$ ${credit}`} text='Total de Créditos'/>
 						<ValueBox cols=' 12 4' color='red' icon='credit-card' value={`R$ ${debt}`} text='Total de Débitos'/>
-						<ValueBox cols=' 12 4' color='blue' icon='money' value='R$ 0' text='Valor consolidado'/>
+						<ValueBox cols=' 12 4' color='blue' icon='money' value={`R$ ${credit-debt}`} text='Valor consolidado'/>
 					</div>
 				</Content>
 			</div>
